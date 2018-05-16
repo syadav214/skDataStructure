b8c1cb9d023511876e29f6cfda4a01b3fe2b2d64
Santoshkumar Yadav <santosh.yadav@BIGTREE.ORG>
2018-05-16 16:49:26 +0530

    Between Two Sets

--- Algo/hr_implement/Array1_IsFactor_ofSome_which_are_factors_Of_Array2.py
+++ Algo/hr_implement/Array1_IsFactor_ofSome_which_are_factors_Of_Array2.py
@@ -1,3 +1,10 @@
+"""
+You will be given two arrays of integers. You will be asked to determine all integers that satisfy the following two conditions:
+
+The elements of the first array are all factors of the integer being considered
+The integer being considered is a factor of all elements of the second array
+"""
+
 from functools import reduce
 
 
