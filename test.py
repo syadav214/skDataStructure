2bf39abe2ff6cdbc5bfeb7fcfabf537153f0d238
Santoshkumar Yadav <santosh.yadav@BIGTREE.ORG>
2018-05-25 11:46:41 +0530

    day of programmer

--- test.py
+++ test.py
@@ -1,18 +1,18 @@
-def migratoryBirds(ar):
-	max_val = max(ar)
-	count_hash = {}	
-	for i in range(len(ar)):
-		if ar[i] in count_hash:
-			prev_count = count_hash[ar[i]]
-			count_hash[ar[i]] = prev_count + 1
-		else:
-			count_hash[ar[i]] = 1
+"""Leap year before 1918 in Russia (Julian calender) was determine by divisble by 4
+	After 1918 i.e. Gregorian calender => leap years determine with Divisible by 400 OR
+	Divisible by 4 and not divisible by 100."""
 	
-	max_occurance_type = max(count_hash,key=count_hash.get)
-	print max_occurance_type
-	print count_hash[max_occurance_type]
+def solve(year):
+	day_of_programmer = ''
+	if year == 1918:
+		day_of_programmer = '26.09.'
+	elif ((year<1918) and (year%4==0)) or ((year%400==0) or ((year%4==0) and (year%100!=0))):
+		day_of_programmer = '12.09.'
+	else:
+		day_of_programmer = '13.09.'
+	
+	return day_of_programmer + str(year)
 
 
 if __name__ == "__main__":
-	ar = [1, 4 ,4, 4 ,5, 3,3]
-	migratoryBirds(ar)
+	print solve(1800)
