90923abe12893f47a57ce822e5ffef9a107eb5e4
Santoshkumar Yadav <santosh.yadav@BIGTREE.ORG>
2018-05-31 14:10:39 +0530

    elect shop money spent

--- Algo/hr_implement/elect_shop_money_spent.py
+++ Algo/hr_implement/elect_shop_money_spent.py
@@ -1,3 +1,5 @@
+elect_shop_money_spent.py
+
 """
 Monica wants to buy a keyboard and a USB drive from her favorite electronics store. 
 The store has several models of each. 
@@ -7,13 +9,13 @@ If she doesn't have enough money to buy one keyboard and one USB drive, print -1
 """
 
 def getMoneySpent(keyboards, drives, b):
-	ans=-1
+	totalSpent=-1
 	for x in keyboards:
 		for y in drives:
 			if x+y<=b:
-				ans=max(ans,x+y)
+				totalSpent=max(totalSpent,x+y)
 	
-	return(ans)
+	return(totalSpent)
 
 if __name__ == "__main__":
 	keyboards =[3, 1]
