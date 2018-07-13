13b18ee73d4d5e5c7e1363ed26123353c73892df
Santoshkumar Yadav <syadav214@gmail.com>
2018-07-13 14:38:26 +0530

    appendAndDelete

--- test.js
+++ test.js
@@ -1,10 +1,27 @@
-function extraLongFactorials(n) {
-    let multi=1, x=n;
-    for(i=0;i<n-1;i++){        
-        multi = multi * x;
-        x = x - 1;
+function appendAndDelete(s, t, k) {
+  if (s.length + t.length < k) {
+    return 'Yes';
+  }
+  let diff = 0;
+  for (let i in s) {
+    if (i >= t.length) {
+      diff = i;
+      break;
+    } else if (s[i] != t[i]) {
+      diff = i;
+      break;
     }
-    return multi;
+  }
+  diff = s.length - diff + (t.length - diff);
+
+  if (diff <= k && (diff - k) % 2 == 0) {
+    return 'Yes';
+  } else {
+    return 'No';
+  }
 }
 
-console.log(extraLongFactorials(5));
+let s = 'hackerhappy',
+  t = 'hackerrank',
+  k = 9;
+console.log(appendAndDelete(s, t, k));
