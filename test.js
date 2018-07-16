41c042761b01f4e0095f53d6afa87377bd941858
Santoshkumar Yadav <syadav214@gmail.com>
2018-07-16 12:17:27 +0530

    squares ranges

--- test.js
+++ test.js
@@ -1,27 +1,5 @@
-function appendAndDelete(s, t, k) {
-  if (s.length + t.length < k) {
-    return 'Yes';
-  }
-  let diff = 0;
-  for (let i in s) {
-    if (i >= t.length) {
-      diff = i;
-      break;
-    } else if (s[i] != t[i]) {
-      diff = i;
-      break;
-    }
-  }
-  diff = s.length - diff + (t.length - diff);
-
-  if (diff <= k && (diff - k) % 2 == 0) {
-    return 'Yes';
-  } else {
-    return 'No';
-  }
+function squares(a,b){
+  return ((Math.floor(Math.sqrt(b)) - Math.ceil(Math.sqrt(a))) + 1);
 }
 
-let s = 'hackerhappy',
-  t = 'hackerrank',
-  k = 9;
-console.log(appendAndDelete(s, t, k));
+console.log(squares(3,9));
