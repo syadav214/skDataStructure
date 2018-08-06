38c83f6932f6cd713ae517a7c36707723d630b65
Santoshkumar Yadav <syadav214@gmail.com>
2018-08-06 18:06:03 +0530

    cpp stl

---
+++ Algo/udemy_cpp/maximumNumber_usingSTL.cpp
@@ -0,0 +1,24 @@
+// STL is standard template library to get method to do repatative tasks
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a = 5, b = 8, maximum;
+    // maximum from a and b
+    maximum = max(a, b);
+
+    //swapping two values
+    swap(a, b); // a=8, b=5
+
+    //double pow (double base, double exponent);
+
+    int number = 2;
+    double cubicRoot;
+
+    cubicRoot = pow((double)(number), 1.0 / 3); // 1(or 3) is of type int / 1.0(or 3.0) is of type double
+
+    cout << fixed << setprecision(10) << cubicRoot << "\n";
+    cout << fixed << setprecision(3) << cubicRoot << "\n";
+
+    return 0;
+}
