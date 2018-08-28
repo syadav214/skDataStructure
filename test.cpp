65c6c0a62bdc0bc534ae7ab03e4c5a1d25f48bf6
Santoshkumar Yadav <syadav214@gmail.com>
2018-08-28 12:38:10 +0530

    moved cpp practice to a new repo

--- test.cpp
+++ test.cpp
@@ -1,47 +1,20 @@
 /* Compile with c++11
 g++ -std=c++11 test.cpp
 */
-/*
-You wish to buy video games from the famous online video game store Mist.
-Usually, all games are sold at the same price, p dollars. 
-However, they are planning to have the seasonal Halloween Sale next month in which you can buy games at a cheaper price.
-Specifically, the first game you buy during the sale will be sold at p dollars, 
-but every subsequent game you buy will be sold at exactly d dollars less than the cost of the previous one you bought.
-This will continue until the cost becomes less than or equal to m dollars, after which every game you buy will cost m dollars each.
-*/
-
 #include <iostream>
-#include <bits/stdc++.h>
-using namespace std;
-
-int howManyGames(int p, int d, int m, int s)
-{
-    if (s < p)
-        return 0;
-    if (s == p)
-        return 1;
-    int count = 1;
-    int sum = p;
-    while (p - d >= m)
-    {
-        p = p - d;
-        if (sum + p > s)
-            return count;
-        else
-            count++;
-        sum += p;
-    }
-    p = m;
-    while (sum + p <= s)
-    {
-        sum = sum + p;
-        count++;
-    }
-    return count;
-}
+#include <cstdio>
 
 int main()
 {
-    cout << "Ans: " << howManyGames(20, 3, 6, 80) << endl;
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+
+    //scanf("%i %li %c %f %lf", &a, &b, &c, &d, &e);
+    //printf("%i\n%li\n%c\n%.03f\n%.09lf\n", a, b, c, d, e);
+    scanf("%i %li", &a, &b);
+    printf("%i\n%li\n", a, b);
     return 0;
 }
