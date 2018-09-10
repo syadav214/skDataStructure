b413f3bb671f85379b4c31117fe9c9662df750b7
Santoshkumar Yadav <syadav214@gmail.com>
2018-09-10 17:57:40 +0530

    tested min num

--- test.cpp
+++ test.cpp
@@ -5,24 +5,72 @@ g++ -std=c++11 test.cpp
 #include <bits/stdc++.h>
 using namespace std;
 
-int camelcase(string s)
+int minimumNumber(int n, string password)
 {
-    int count = 1;
-
-    for (int i = 0; i < s.size(); i++)
+    int lenReq = n;
+    bool numbers = false, lowercase = false, uppercase = false, speacialChar = false;
+    // 48 to 57
+    // 65 to 90
+    // 97 to 122
+    for (int i = 0; i < n; i++)
     {
-        int asciiVal = (int)s[i];
-        if (asciiVal >= 65 && asciiVal <= 90)
+        int asciiVal = (int)password[i];
+        if (asciiVal >= 48 && asciiVal <= 57)
         {
-            count++;
+            numbers = true;
+        }
+        else if (asciiVal >= 65 && asciiVal <= 90)
+        {
+            uppercase = true;
+        }
+        else if (asciiVal >= 97 && asciiVal <= 122)
+        {
+            lowercase = true;
+        }
+        else
+        {
+            speacialChar = true;
         }
     }
 
-    return count;
+    int cnt = 0;
+    if (numbers)
+    {
+        cnt++;
+    }
+
+    if (lowercase)
+    {
+        cnt++;
+    }
+
+    if (uppercase)
+    {
+        cnt++;
+    }
+
+    if (speacialChar)
+    {
+        cnt++;
+    }
+
+    cout << numbers << " " << lowercase << " " << uppercase << " " << speacialChar << endl;
+    int diff = 4 - cnt;
+    cout << diff;
+
+    if (n > 5 && diff > 0)
+    {
+        return diff;
+    }
+    else
+    {
+
+        return 6 - cnt;
+    }
 }
 
 int main()
 {
-    cout << "Ans: " << camelcase("saveChangesInTheEditor") << endl;
+    cout << "Ans: " << minimumNumber(11, "#1HackerRank") << endl;
     return 0;
 }
