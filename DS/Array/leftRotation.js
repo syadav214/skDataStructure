58d9010f3fce2f611817df3e1c5b2a63cbef0407
Santoshkumar Yadav <syadav214@gmail.com>
2018-12-06 11:12:25 +0530

    Renamed variable

--- DS/Array/leftRotation.js
+++ DS/Array/leftRotation.js
@@ -3,38 +3,38 @@
 process.stdin.resume();
 process.stdin.setEncoding('ascii');
 
-var input_stdin = "";
-var input_stdin_array = "";
+var input_stdin = '';
+var input_stdin_array = '';
 var input_currentline = 0;
 
 //console.log('Ctrl + C to get the result===>')
 
 process.stdin.on('data', function(data) {
-    input_stdin += data;
+  input_stdin += data;
 });
 
 process.on('SIGINT', function() {
-    input_stdin_array = input_stdin.split("\n");
-    main();
-    process.exit();
+  input_stdin_array = input_stdin.split('\n');
+  main();
+  process.exit();
 });
 
 function readLine() {
-    return input_stdin_array[input_currentline++];
+  return input_stdin_array[input_currentline++];
 }
 
 /////////////// ignore above this line ////////////////////
 
 function main() {
-    d = parseInt(readLine());
-    a = readLine().split(' ');
-    finalArray = new Array(lenArray);
-    for (i = 0; i < a.length; i++) {
-        var index = i - d;
-        if (index < 0) {
-            index = a.length + index;
-        }
-        finalArray[index] = a[i];
+  d = parseInt(readLine());
+  a = readLine().split(' ');
+  finalArray = new Array(lenArray);
+  for (i = 0; i < a.length; i++) {
+    var index = i - d;
+    if (index < 0) {
+      index = a.length + index;
     }
-    console.log(finalArra);
+    finalArray[index] = a[i];
+  }
+  console.log(finalArray);
 }
