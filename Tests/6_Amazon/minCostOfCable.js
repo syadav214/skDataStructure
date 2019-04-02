196c50f4e40e89b319d881ff838b5eb5e918c53a
Santoshkumar Yadav <syadav214@gmail.com>
2019-04-02 13:49:22 +0530

    createIndividualFilesfromJSON

---
+++ Tests/6_Amazon/minCostOfCable.js
@@ -0,0 +1,11 @@
+/*
+Find minimum cost of connecting cables of all the edges
+*/
+const numTotalEdgeNodes = 6,
+  numTotalAvailableNetworkRoutes = 3,
+  networkRoutesAvailable = [[1, 4], [4, 5], [2, 3]],
+  numNewNetworkRoutesConstruct = 4,
+  costNewNetworkRoutesConstruct = [[1, 2, 5], [1, 3, 10], [1, 6, 2], [5, 6, 5]];
+
+  
+  
