4d395d0ba5a812c3273fc09bb3f9b266d613863b
Santoshkumar Yadav <syadav214@gmail.com>
2019-05-07 17:21:30 +0530

    new logic

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 
 #integer_on_Ticket = [0, -1, -2, 0, 0, 0]
-integer_on_Ticket = [-1, 7, 8, -5, 4]
-integer_on_Ticket = [3, 2, 1, -1]
-integer_on_Ticket = [11, 12, -2, -1]
-integer_on_Ticket = [3, 4, 5, 4]
+integer_on_Ticket = [1,-1, 7, 0, 8, -5, 4]
+#integer_on_Ticket = [3, 2, 1, -1]
+#integer_on_Ticket = [11, 12, -2, -1]
+#integer_on_Ticket = [3, 4, 5, 4]
 #integer_on_Ticket = [4, 5, 4, 3]
 #integer_on_Ticket = [5, 10, 4, -1]
 #integer_on_Ticket = [2,3,1,4,5]
@@ -14,49 +14,26 @@ eleIndex = sorted(range(len(integer_on_Ticket)),
 new_integer_on_ticket = integer_on_Ticket[:]
 new_integer_on_ticket.sort(reverse=True)
 
+maxSum = 0
+
+for i in range(N):
+    currEle = new_integer_on_ticket[i]
+    if currEle > 0:
+        currSum = currEle
+        sortedIndex = eleIndex[i]
+        # Backwards
+        print('currEle', currEle)
+        for b in range(sortedIndex - 1, -1, -1):
+            bEle = integer_on_Ticket[b]
+            if(bEle == 0):
+                continue
+            print('b', integer_on_Ticket[b])
+
+        for f in range(sortedIndex + 1, N):
+            fEle = integer_on_Ticket[f]
+            if(fEle == 0):
+                continue
+            print('f', integer_on_Ticket[f])
+
 
 print(integer_on_Ticket, new_integer_on_ticket, eleIndex)
-
-maxVal = 0
-minVal = 0
-printingTicketVal = []
-x = N if(N < 100) else 1
-for mainIndex in range(x):
-    prevVal = new_integer_on_ticket[mainIndex]
-    printingTicketIndex = []
-    currVal = 0
-    if(prevVal > 0):
-        prevIndex = eleIndex[mainIndex]
-        evenOrOdd = prevIndex % 2 == 0        
-        printingTicketIndex.append(prevIndex)        
-        currVal += prevVal
-        adjIds = []
-        adjIds.append(prevIndex+1)
-        adjIds.append(prevIndex-1)
-        for i in range(N):
-            if (integer_on_Ticket[i] == 0):
-                continue
-
-            if(i == 0 and evenOrOdd == False):
-                continue
-
-            if (prevIndex != i and integer_on_Ticket[i] > 0 and i not in adjIds):
-                currVal += integer_on_Ticket[i]
-                printingTicketIndex.append(i)
-                adjIds.append(i+1)
-                adjIds.append(i-1)
-        if(currVal > maxVal):
-            maxVal = currVal
-            minVal = integer_on_Ticket[max(printingTicketIndex)]
-            printingTicketVal = printingTicketIndex
-        elif currVal == maxVal:
-            maxOfMinVal = integer_on_Ticket[max(printingTicketIndex)]
-            if(maxOfMinVal > minVal):
-                minVal = maxOfMinVal
-                printingTicketVal = printingTicketIndex
-
-printingTicketVal.sort(reverse=True)
-finalStr = ''
-for currEle in printingTicketVal:
-    finalStr += str(integer_on_Ticket[currEle])
-print(finalStr)
