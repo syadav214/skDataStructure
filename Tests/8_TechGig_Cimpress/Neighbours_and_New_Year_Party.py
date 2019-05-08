6e7848af563d30d77b76efa70682c2c0b9ec78b3
Santoshkumar Yadav <syadav214@gmail.com>
2019-05-08 12:24:21 +0530

    get ticket no

--- Tests/8_TechGig_Cimpress/Neighbours_and_New_Year_Party.py
+++ Tests/8_TechGig_Cimpress/Neighbours_and_New_Year_Party.py
@@ -1,26 +1,65 @@
-def getTicketNo(integer_on_Ticket):    
-    sumIncluingPrevElement = 0
-    sumExcluingPrevElement = 0
-    elementArr = []
-    ticketNo = ''
+''' Read input from STDIN. Print your output to STDOUT '''
+    #Use input() to read input from STDIN and use print to write your output to STDOUT
 
-    for currElement in integer_on_Ticket:
-        new_excl = max(sumIncluingPrevElement, sumExcluingPrevElement)
-        sumIncluingPrevElement = sumExcluingPrevElement + currElement
-        elementArr.append([sumExcluingPrevElement, currElement])
-        sumExcluingPrevElement = new_excl
-    
-    maxSum = max(sumIncluingPrevElement, sumExcluingPrevElement)
-    for ele in elementArr:
-        if(ele[0]+ele[1] == maxSum):
-            if(ele[0] == 0):
-                ticketNo = str(ele[1])
-            else:
-                ticketNo = str(ele[1]) + str(ele[0])
+def getTicketNo(N,integer_on_Ticket):
+    eleIndex = sorted(range(len(integer_on_Ticket)),key=lambda k: integer_on_Ticket[k], reverse=True)
+    new_integer_on_ticket = integer_on_Ticket[:]
+    new_integer_on_ticket.sort(reverse=True)
+    maxSum = 0
+    ticketPrint = []
+
+    for i in range(N):
+        currEle = new_integer_on_ticket[i]
+        if currEle > 0:
+            printArr = []
+            currSum = currEle
+            sortedIndex = eleIndex[i]
+            printArr.append(sortedIndex)
+            validIndex = sortedIndex - 1
+            # Backwards
+            for b in range(sortedIndex - 1, -1, -1):
+                bEle = integer_on_Ticket[b]
+                if(bEle == 0):
+                    validIndex = validIndex - 1
+                    continue
+                if(b == validIndex):
+                    continue
+                validIndex = b - 1
+                if(bEle > 0):
+                    currSum += bEle
+                    printArr.append(b)
+
+            validIndex = sortedIndex + 1
+            # Forwards
+            for f in range(sortedIndex + 1, N):
+                fEle = integer_on_Ticket[f]
+                if(fEle == 0):
+                    validIndex = validIndex + 1
+                    continue
+                if(f == validIndex):
+                    continue
+                validIndex = f + 1
+                if(fEle > 0):
+                    currSum += fEle
+                    printArr.append(f)
+            
+            if(currSum > maxSum):
+                maxSum = currSum
+                ticketPrint = printArr
+            elif currSum == maxSum:
+                maxLastEle = integer_on_Ticket[max(ticketPrint)]
+                currMaxLastEle = integer_on_Ticket[max(printArr)]
+                if(currMaxLastEle > maxLastEle):
+                    ticketPrint = printArr
+
+        else:
             break
-    
-    return ticketNo
 
+    ticketPrint.sort(reverse=True)
+    finalStr = ''
+    for currEle in ticketPrint:
+        finalStr += str(integer_on_Ticket[currEle])
+    print(finalStr)
 
 def main():
 
@@ -28,7 +67,8 @@ def main():
     for i in range(T):
         N = int(input())
         integer_on_Ticket = list(map(int, input().split()))
-        print(getTicketNo(integer_on_Ticket))
+        getTicketNo(N,integer_on_Ticket)
+
 
 main()
 
--- test.py
+++ test.py
@@ -1,39 +1,71 @@
 
 #integer_on_Ticket = [0, -1, -2, 0, 0, 0]
-integer_on_Ticket = [1,-1, 7, 0, 8, -5, 4]
+#integer_on_Ticket = [1, -1, 0, 7, 8, -5, 4]
+#integer_on_Ticket = [-1, 7, 8, -5, 0, 4]
+integer_on_Ticket = [-1, 7, 8, -5, 4]
 #integer_on_Ticket = [3, 2, 1, -1]
 #integer_on_Ticket = [11, 12, -2, -1]
 #integer_on_Ticket = [3, 4, 5, 4]
 #integer_on_Ticket = [4, 5, 4, 3]
-#integer_on_Ticket = [5, 10, 4, -1]
-#integer_on_Ticket = [2,3,1,4,5]
+integer_on_Ticket = [5, 10, 4, -1]
+integer_on_Ticket = [2,3,1,4,5]
 N = len(integer_on_Ticket)
 
-eleIndex = sorted(range(len(integer_on_Ticket)),
-                  key=lambda k: integer_on_Ticket[k], reverse=True)
+eleIndex = sorted(range(len(integer_on_Ticket)),key=lambda k: integer_on_Ticket[k], reverse=True)
 new_integer_on_ticket = integer_on_Ticket[:]
 new_integer_on_ticket.sort(reverse=True)
 
+print(integer_on_Ticket, new_integer_on_ticket, eleIndex)
+
 maxSum = 0
+ticketPrint = []
 
 for i in range(N):
     currEle = new_integer_on_ticket[i]
     if currEle > 0:
+        printArr = []
         currSum = currEle
         sortedIndex = eleIndex[i]
+        printArr.append(sortedIndex)
+        validIndex = sortedIndex - 1
         # Backwards
-        print('currEle', currEle)
         for b in range(sortedIndex - 1, -1, -1):
             bEle = integer_on_Ticket[b]
             if(bEle == 0):
+                validIndex = validIndex - 1
                 continue
-            print('b', integer_on_Ticket[b])
+            if(b == validIndex):
+                continue
+            validIndex = b - 1
+            if(bEle > 0):
+                currSum += bEle
+                printArr.append(b)
 
+        validIndex = sortedIndex + 1
+        # Forwards
         for f in range(sortedIndex + 1, N):
             fEle = integer_on_Ticket[f]
             if(fEle == 0):
+                validIndex = validIndex + 1
                 continue
-            print('f', integer_on_Ticket[f])
+            if(f == validIndex):
+                continue
+            validIndex = f + 1
+            if(fEle > 0):
+                currSum += fEle
+                printArr.append(f)
 
+        if(currSum > maxSum):
+            maxSum = currSum
+            ticketPrint = printArr
+        elif currSum == maxSum:
+            maxLastEle = integer_on_Ticket[max(ticketPrint)]
+            currMaxLastEle = integer_on_Ticket[max(printArr)]
+            if(currMaxLastEle > maxLastEle):
+                ticketPrint = printArr
 
-print(integer_on_Ticket, new_integer_on_ticket, eleIndex)
+ticketPrint.sort(reverse=True)
+finalStr = ''
+for currEle in ticketPrint:
+    finalStr += str(integer_on_Ticket[currEle])
+print(finalStr)
