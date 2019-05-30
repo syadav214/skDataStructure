b5678312da59f8d34e4938e0f6761a5210a0cd33
Santoshkumar Yadav <syadav214@gmail.com>
2019-05-30 15:19:39 +0530

    tg election

--- test.py
+++ test.py
@@ -1,11 +1,19 @@
-phases = [3, 0, 0]
-states = [3, 0, 0]
-phases = [3, 2, 1]
-states = [1, 2, 2]
+phases = [3, 1, 0]
+states = [2, 2, 0]
+phases = [4, 3, 1]
+states = [3, 2, 3]
+phases = [3, 1, 3]
+states = [3, 3, 1]
 phases = [0, 1, 2, 2]
 states = [2, 3]
 phases = [2, 1, 0]
 states = [1, 2]
+phases = [3, 2, 1]
+states = [1, 2, 2]
+phases = [3, 0, 0]
+states = [3, 0, 0]
+phases = [1, 0, 0]
+states = [0, 0, 0]
 
 maxInPhases = max(phases)
 totInPhases = sum(phases)
@@ -14,13 +22,13 @@ for i in phases:
     if(i > 0):
         possibleWinInPhase += 1
 
-
 maxInStates = max(states)
 totInStates = sum(states)
 possibleWinInState = 0
 for j in states:
     if(j > 0):
         possibleWinInState += 1
+
 
 if(totInPhases == totInStates and possibleWinInPhase >= maxInStates and possibleWinInState >= maxInPhases):
     print('YES')
