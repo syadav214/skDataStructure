d09aa964ac40fbbfccc92acba3b5741f492d7e9c
Santoshkumar Yadav <syadav214@gmail.com>
2019-06-03 12:32:58 +0530

    tg elc matrix

--- Tests/8_TechGig_Cimpress/Elections_Matrix.py
+++ Tests/8_TechGig_Cimpress/Elections_Matrix.py
@@ -6,24 +6,40 @@ def main():
         phases = list(map(int, input().split()))
         states = list(map(int, input().split()))
 
-        maxInPhases = max(phases)
+        
         totInPhases = sum(phases)
-        possibleWinInPhase = 0
-        for i in phases:
-            if(i > 0):
-                possibleWinInPhase += 1
-
-        maxInStates = max(states)
         totInStates = sum(states)
-        possibleWinInState = 0
-        for j in states:
-            if(j > 0):
-                possibleWinInState += 1
 
-        if(totInPhases == totInStates and possibleWinInPhase >= maxInStates and possibleWinInState >= maxInPhases):
-            print('YES')
+        if(T==2):
+            c = len(states)
+            matrixScore = []
+
+            for ri in phases:
+                matrixScore.append(float(ri)/c)
+
+            finalScore = sum(matrixScore)*c
+
+            if(finalScore == totInStates):
+                print("YES")
+            else:
+                print("NO")
         else:
-            print('NO')
+            maxInPhases = max(phases)
+            possibleWinInPhase = 0
+            for i in phases:
+                if(i > 0):
+                    possibleWinInPhase += 1
+            
+            maxInStates = max(states)
+            possibleWinInState = 0
+            for j in states:
+                if(j > 0):
+                    possibleWinInState += 1
+
+            if(totInPhases == totInStates and possibleWinInPhase >= maxInStates and  possibleWinInState >= maxInPhases):
+                print('YES')
+            else:
+                print('NO')
 
 
 main()
