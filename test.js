a2cf641a1bd6ee4bfda19c4f2f4660cb79f3d8cf
Santoshkumar Yadav <syadav214@gmail.com>
2019-12-03 19:45:37 +0530

    quicksort js

--- test.js
+++ test.js
@@ -1,19 +1,30 @@
-const arr = [1, 3, 10, 2, 11, 4, 12];
-const arrSet = new Set(arr);
-let max = 0;
-for (let ele of arrSet) {
-  let count = 1;
-  while (true) {
-    ele++;
-    if (arrSet.has(ele)) {
-      count++;
-    } else {
-      break;
+const arr = [5, 8, 3, 2, 4];
+quickSort(arr, 0, arr.length - 1);
+console.log('arr', arr);
+
+function partition(arr, low, high) {
+  let pi = arr[high];
+  let index = low - 1;
+
+  for (let j = low; j <= high - 1; j++) {
+    if (arr[j] <= pi) {
+      index++;
+      let temp = arr[index];
+      arr[index] = arr[j];
+      arr[j] = temp;
     }
   }
-  if (count > max) {
-    max = count;
-  }
+  let temp = arr[index + 1];
+  arr[index + 1] = arr[high];
+  arr[high] = temp;
+
+  return index + 1;
 }
 
-console.log(max);
+function quickSort(arr, low, high) {
+  if (low < high) {
+    let pi = partition(arr, low, high);
+    quickSort(arr, low, pi - 1);
+    quickSort(arr, pi + 1, high);
+  }
+}
