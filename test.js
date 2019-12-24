719aa24ddc1cf20bb54df5b33e66dccc64603d54
Santoshkumar Yadav <syadav214@gmail.com>
2019-12-24 19:05:18 +0530

    activity[0]

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const activity = [
-  { name: 'a1', start: 0, end: 6 },
+  { name: 'a1', start: 0, end: 2 },
   { name: 'a2', start: 3, end: 4 },
   { name: 'a3', start: 1, end: 2 },
   { name: 'a4', start: 5, end: 9 },
@@ -7,7 +7,19 @@ const activity = [
   { name: 'a6', start: 8, end: 9 }
 ];
 
-//sort
+activity.sort((a, b) => a.start - b.start);
+
+console.log(activity[0].name);
+let prevEnd = activity[0].end;
+
+for (let i = 1; i < activity.length; i++) {
+  if(activity[i].start > prevEnd) {
+    console.log(activity[i].name);
+    prevEnd = activity[i].end;
+  }
+}
+
+
 //compare each finish with next start and print
 
 //https://www.youtube.com/watch?v=poWB2UCuozA
