4a29afc5d2c7f633e2557295cb72712fa010f184
Santoshkumar Yadav <syadav214@gmail.com>
2021-04-28 18:47:30 +0530

    burst ballons

---
+++ Tests/13_sports_2021/minCoinChange.java
@@ -0,0 +1,41 @@
+
+import java.io.*;
+import java.util.*;
+
+class test {
+    
+
+    public static void main(String[] args) {        
+        int coins[] = { 1,2,5};
+        System.out.println(coinChange(coins, 11));
+    }
+
+    static int coinChange(int[] coins, int amount) {
+        Arrays.sort(coins);
+        if(amount == 0){
+            return 0;
+        }
+        int[] dp = new int[amount+1];
+        Arrays.fill(dp,amount+1);
+        dp[0]=0;
+        
+        for(int j=0;j< coins.length;j++){
+            System.out.println("==========================");
+            System.out.print("coins[j]: ");
+            System.out.println(coins[j]);
+            
+               for(int i=coins[j];i<=amount;i++){
+                System.out.print("i: ");
+                System.out.println(i);
+                System.out.print("dp[i]: ");
+                System.out.println(dp[i]);
+                System.out.print("1 + dp[i-coins[j]]: ");
+                System.out.println(1 + dp[i-coins[j]]);
+                dp[i] = Math.min(dp[i], 1 + dp[i-coins[j]]);  
+            }
+        }
+        
+        return dp[amount] > amount ? -1 : dp[amount];        
+    }
+}
+
