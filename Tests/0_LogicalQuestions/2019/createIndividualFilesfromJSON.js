eb7a448b94d0c7ad79af48d07058b67102c349f8
Santosh Yadav <syadav214@gmail.com>
2021-07-20 11:29:11 +0530

    removed

--- Tests/0_LogicalQuestions/2019/createIndividualFilesfromJSON.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-let rawdata = fs.readFileSync('blogData.json');
-let student = JSON.parse(rawdata);
-for (let s in student) {
-  fs.writeFileSync('test/' + s, student[s]);
-  //console.log(s,student[s]);
-}
